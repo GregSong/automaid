aa8fcb997f8b3138e155b75c4930ce63293ee708
Greg Song <kuroro2121@gmail.com>
2014-10-27 16:50:01 +0800

    There is a autoload issue that autoloader failed to get correct path of automaid.

--- autoload.php
+++ autoload.php
@@ -11,5 +11,7 @@ if (file_exists(__DIR__.'/vendor/autoload.php')) {
     $loader->add('Codeception', __DIR__ . '/src');
     $loader->register(true);
 } elseif (file_exists(__DIR__.'/../../autoload.php')) {
-    require_once __DIR__ . '/../../autoload.php';
+    $loader = (require_once __DIR__ . '/../../autoload.php');
 }
+
+return $loader;
