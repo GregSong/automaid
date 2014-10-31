720767a4d8d416e49742bc8c6ae9c1b36a37a86a
Greg Song <kuroro2121@gmail.com>
2014-10-31 22:26:13 +0800

    introduce dir trait to simplify code

---
+++ src/AutoMaid/DirTrait.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kuroro2121
+ * Date: 14/10/31
+ * Time: 22:13
+ */
+
+namespace AutoMaid;
+
+
+trait DirTrait {
+    protected $projectDir;
+    protected $projectSrc;
+
+    /**
+     * @return mixed
+     */
+    public function getProjectDir()
+    {
+        return $this->projectDir;
+    }
+
+    /**
+     * @param mixed $projectDir
+     */
+    public function setProjectDir($projectDir)
+    {
+        $this->projectDir = $projectDir;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getProjectSrc()
+    {
+        return $this->projectSrc;
+    }
+
+    /**
+     * @param mixed $projectSrc
+     */
+    public function setProjectSrc($projectSrc)
+    {
+        $this->projectSrc = $projectSrc;
+    }
+
+    public function detectProjectDir()
+    {
+        $this->projectDir = dirname(dirname(dirname(dirname(dirname(dirname((__DIR__)))))));
+        $this->projectSrc = $this->projectDir . '/src';
+    }
+
+} 
