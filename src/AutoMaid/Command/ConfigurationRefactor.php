8d4c52fe0c6ba5eb8d152d03c12598da933fba4c
Greg Song <kuroro2121@gmail.com>
2014-11-01 17:56:02 +0800

    Add missing use

--- src/AutoMaid/Command/ConfigurationRefactor.php
+++ src/AutoMaid/Command/ConfigurationRefactor.php
@@ -10,6 +10,8 @@ namespace AutoMaid\Command;
 
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
 class ConfigurationRefactor extends Command{
     protected function configure()
