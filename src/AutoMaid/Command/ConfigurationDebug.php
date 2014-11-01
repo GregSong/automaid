4f160f20571160e7bca480b161d6c2c0a3592178
Greg Song <kuroro2121@gmail.com>
2014-11-01 14:32:46 +0800

    now debug can print detail information about a service definition

--- src/AutoMaid/Command/ConfigurationDebug.php
+++ src/AutoMaid/Command/ConfigurationDebug.php
@@ -15,10 +15,13 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\Yaml\Yaml;
 
 class ConfigurationDebug extends Command
 {
     use DirTrait;
+
     protected function configure()
     {
         $this->setDefinition(
@@ -50,6 +53,38 @@ class ConfigurationDebug extends Command
 
         $automaid->loadFiles($this->projectSrc);
         $automaid->parseServices();
-        echo $automaid->getServiceInfo($serviceName);
+        $definition = $automaid->getServiceInfo($serviceName);
+
+        // TODO Greg: by default just dump the detail of definition
+        echo Yaml::dump($this->dumpDefinition($definition));
+    }
+
+    /**
+     * @param $definition
+     *
+     * @return array
+     */
+    private function dumpDefinition(Definition $definition)
+    {
+        $data = array(
+            'class'     => (string)$definition->getClass(),
+            'scope'     => $definition->getScope(),
+            'public'    => $definition->isPublic(),
+            'synthetic' => $definition->isSynthetic(),
+            'file'      => $definition->getFile(),
+        );
+
+        $data['tags'] = array();
+        if (count($definition->getTags())) {
+            foreach ($definition->getTags() as $tagName => $tagData) {
+                foreach ($tagData as $parameters) {
+                    $data['tags'][] = array('name'       => $tagName,
+                                            'parameters' => $parameters
+                    );
+                }
+            }
+        }
+
+        return $data;
     }
 }
